2360cc5c23412ef6c610ebbfda7c210b6215dab3
Robert Alan <robert.freitasoliveira@hotmail.com>
2024-03-08 11:45:10 -0300

    Aterações nas versões no package, e alteração no codigo gulpfile, com correções de alguns erros

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,32 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
 // Tarefa para compilar o SASS
-gulp.task('sass', function () {
+function compilaSass() {
   return gulp.src('src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist/css'));
-});
+};
 
 // Tarefa para comprimir as imagens
-gulp.task('imagemin', function () {
+function comprimeImagens() {
   return gulp.src('gulp_img/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/images'));
-});
+};
 
 // Tarefa para comprimir o JavaScript
-gulp.task('uglify', function () {
+function comprimeJavaScript() {
   return gulp.src('src/js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'));
-});
+};
 
 // Tarefa padrão que executa todas as tarefas
-gulp.task('default', gulp.series('sass', 'imagemin', 'uglify'));
+exports.default = function() {
+gulp.watch('./src/sass/*.scss', { ignoreInitial: false }, gulp.series (compilaSass)); 
+gulp.watch('./src/js/*.js', { ignoreInitial: false }, gulp.series (comprimeJavaScript)); 
+gulp.watch('./gulp-img/*', { ignoreInitial: false }, gulp.series (comprimeImagens));
+}
